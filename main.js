112c1c6f574e9bb7b2d278b69471c232ae201485
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-10-05 15:51:35 +0530

    chore: adding grid and image component example

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ import {
   LayoutProps, 
   FlexboxProps,
   BorderProps,
+  GridProps,
 } from './dist/bundle.js'
 
 
-class FWBox extends LayoutProps(TypographyProps(ColorProps(SpaceProps(HTMLElement)))) {
+const Box = GridProps(BorderProps(LayoutProps(TypographyProps(ColorProps(SpaceProps(HTMLElement))))))
+
+class FWBox extends Box {
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
@@ -35,6 +38,20 @@ class FWFlex extends FlexboxProps(HTMLElement) {
 
 customElements.define('fw-flex', FWFlex)
 
+class FWGrid extends GridProps(HTMLElement) {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+    const template = document.createElement('template')
+    template.innerHTML = '<div><slot></slot></div>'
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.container = this.shadowRoot.querySelector('div')
+    this.container.style.display = 'grid'
+  }
+}
+
+customElements.define('fw-grid', FWGrid)
+
 class MyCard extends ColorProps(BorderProps(LayoutProps(SpaceProps(HTMLElement)))) {
   constructor() {
     super();
@@ -48,3 +65,27 @@ class MyCard extends ColorProps(BorderProps(LayoutProps(SpaceProps(HTMLElement))
 }
 
 customElements.define('my-card', MyCard);
+
+class BlockText extends TypographyProps(HTMLElement) {
+constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' })
+    const template = document.createElement('template')
+    template.innerHTML = '<p><slot></slot></p>'
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.container = this.shadowRoot.querySelector('p')
+  }
+}
+
+customElements.define('block-text', BlockText);
+
+class Avatar extends BorderProps(HTMLImageElement) {
+  constructor() {
+    super()
+    this.container = this;
+
+  }
+}
+
+customElements.define('fw-avatar', Avatar, { extends: 'img' })
