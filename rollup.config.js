e0e8b85e9557a273a4c510d8ed50179d269ee598
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-10-08 16:18:02 +0530

    chore: update rollup config to run server only in dev env

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,31 @@ import { terser } from 'rollup-plugin-terser'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
+const prod = process.env.NODE_ENV === 'prod'
+
 export default {
-    input: 'src/index.js',
-    output: [
-        {
-            file: 'dist/styled-web-components.min.js',
-        },
-        {
-            file: 'dist/styled-web-components.js',
-            format: 'cjs',
-        },
-        {
-            name: 'styled-web-components',
-            file: 'dist/styled-web-components.umd.min.js',
-            format: 'umd',
-        },
-    ],
-    plugins: [
-        process.env.NODE_ENV === 'prod' && terser(),
-        serve({
-            contentBase: ['dist'],
-            open: true,
-        }),
-        livereload(),
-    ],
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/styled-web-components.min.js',
+    },
+    {
+      file: 'dist/styled-web-components.js',
+      format: 'cjs',
+    },
+    {
+      name: 'styled-web-components',
+      file: 'dist/styled-web-components.umd.min.js',
+      format: 'umd',
+    },
+  ],
+  plugins: [
+    prod && terser(),
+    !prod &&
+      serve({
+        contentBase: ['dist'],
+        open: true,
+      }),
+    !prod && livereload(),
+  ],
 }
